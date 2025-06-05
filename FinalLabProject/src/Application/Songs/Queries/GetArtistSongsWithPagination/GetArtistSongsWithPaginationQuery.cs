80369bcf3682fd7b99802dd60f0150fb164f151f
karolbronowski <karol.bronowski@me.com>
2025-06-05 17:41:45 +0200

    feat: implement GetArtistSongsWithPagination query and validator for songs commands - related to artist

---
+++ FinalLabProject/src/Application/Songs/Queries/GetArtistSongsWithPagination/GetArtistSongsWithPaginationQuery.cs
@@ -0,0 +1,35 @@
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using FinalLabProject.Application.Common.Interfaces;
+using FinalLabProject.Application.Common.Models;
+using MediatR;
+
+namespace FinalLabProject.Application.Songs.Queries.GetArtistSongsWithPagination;
+
+public record GetArtistSongsWithPaginationQuery : IRequest<PaginatedList<SongDto>>
+{
+    public int ArtistId { get; init; }
+    public int PageNumber { get; init; } = 1;
+    public int PageSize { get; init; } = 10;
+}
+
+public class GetArtistSongsWithPaginationQueryHandler : IRequestHandler<GetArtistSongsWithPaginationQuery, PaginatedList<SongDto>>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetArtistSongsWithPaginationQueryHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<PaginatedList<SongDto>> Handle(GetArtistSongsWithPaginationQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.Songs
+            .Where(x => x.ArtistId == request.ArtistId)
+            .OrderBy(x => x.Title)
+            .ProjectTo<SongDto>(_mapper.ConfigurationProvider)
+            .PaginatedListAsync(request.PageNumber, request.PageSize);
+    }
+}
